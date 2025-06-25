d9cf0ea3dfc82594c3ab81b3c499d00fb4ce9b8a
sbx_user1051 <sbx_user1051@169.254.19.121>
2025-06-25 19:25:53 +0000

    Wed Jun 25 19:25:53 UTC 2025

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -1,6 +1,27 @@
 
 class CheckoutSolution:
 
+    #     Our price table and offers:
+    # +------+-------+----------------+
+    # | Item | Price | Special offers |
+    # +------+-------+----------------+
+    # | A    | 50    | 3A for 130     |
+    # | B    | 30    | 2B for 45      |
+    # | C    | 20    |                |
+    # | D    | 15    |                |
+    # +------+-------+----------------+
+
+# Requirements
+#  - Our goods are priced individually. In addition, some items are multi-priced: buy n of them, and they'll cost you y pounds.
+#  - For example, item A might cost 50 pounds individually, but this week we have a special offer:
+#  - buy three As and they'll cost you 130.
+#  - For any illegal input return -1
+
     # skus = unicode string
     def checkout(self, skus):
+        """
+        - param[0] = a string containing the SKUs of all the products in the basket
+        - @return = an integer representing the total checkout value of the items
+        """
         raise NotImplementedError()
+
