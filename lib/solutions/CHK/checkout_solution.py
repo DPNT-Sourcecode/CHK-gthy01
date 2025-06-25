ad510f9f298a99e9403b0a46d49e97cf496df2e1
sbx_user1051 <sbx_user1051@169.254.19.121>
2025-06-25 20:46:53 +0000

    Wed Jun 25 20:46:53 UTC 2025

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -41,6 +41,7 @@ class CheckoutSolution:
 
         if pricing["A"]:
             breakpoint()
+            if pricing["A"]
             offers = (pricing["A"][1] // 3)
             
             total_price += ()
@@ -50,12 +51,6 @@ class CheckoutSolution:
             # TODO: Will need to adjust this afterwards with n SKUs which don't meet the offer limit
             # e.g. 5 (3 == 130 + 2 == 100) total = 230
             # Probably want to handle this within the looping?
-
-
-
-
-
-
 
 
 
