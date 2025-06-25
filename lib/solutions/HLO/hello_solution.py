5f2023dc49924b0c7269d7b6f13c3b738483508d
sbx_user1051 <sbx_user1051@169.254.19.121>
2025-06-25 19:03:07 +0000

    Wed Jun 25 19:03:07 UTC 2025

--- lib/solutions/HLO/hello_solution.py
+++ lib/solutions/HLO/hello_solution.py
@@ -17,7 +17,6 @@ class HelloSolution:
 
     def valid_name(self, name: str) -> str | None:
         """ Responsible for managing when the name provided is valid"""
-        # rejected_names = ["Mr. X", "Craftsman"]
         accepted_names = ["John", "Mr. X", "Craftsman", "Adam"]
         if name in accepted_names:
             return name
