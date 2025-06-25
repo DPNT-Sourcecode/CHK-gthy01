89a5237bd1ca7e57323ea090e1c0e51b2abd1474
sbx_user1051 <sbx_user1051@169.254.19.121>
2025-06-25 18:30:14 +0000

    Wed Jun 25 18:30:14 UTC 2025

--- lib/solutions/HLO/hello_solution.py
+++ lib/solutions/HLO/hello_solution.py
@@ -2,6 +2,16 @@
 class HelloSolution:
     
     # friend_name = unicode string
-    def hello(self, friend_name):
+    def hello(self, friend_name: str) -> str:
+        """
+         - param[0] = a String containing a name
+         - @return = a String containing a message
+        """
+        
+        # Could add some handling around if str is empty
+        # if friend_name == "":
+
+
         return f"Hello {friend_name}"
+
 
