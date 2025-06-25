4417354b78f7851b521bbf13bbc97e44ac88c696
sbx_user1051 <sbx_user1051@169.254.19.121>
2025-06-25 18:05:36 +0000

    Wed Jun 25 18:05:36 UTC 2025

--- lib/solutions/SUM/sum_solution.py
+++ lib/solutions/SUM/sum_solution.py
@@ -1,5 +1,14 @@
 
 class SumSolution:
-    
+
     def compute(self, x, y):
-        raise NotImplementedError()
+        """
+        Sum recieves inputs num_1 and num_2 and returns addition
+        Sum does not accept values outside the bounds of 0-100
+        """
+
+        if (x < 0 or y < 0) or (x > 100 or y > 100):
+            raise(ValueError) 
+
+        return x + y
+
