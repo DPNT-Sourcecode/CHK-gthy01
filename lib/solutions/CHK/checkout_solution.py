e5637d8532cbc809b5d5336eba17ff30bd9df8c5
sbx_user1051 <sbx_user1051@169.254.19.121>
2025-06-25 21:31:14 +0000

    Wed Jun 25 21:31:14 UTC 2025

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -25,6 +25,7 @@ class CheckoutSolution:
             return 0
         skus_on_offer = ["A", "B", "E"]
         pricing: dict[str, tuple[int, int]] = { "A": (50, 0), "B": (30, 0), "C": (20, 0), "D": (15, 0), "E": (40, 0) }
+        offers = 0
 
         total_price = 0
         for sku in skus:
@@ -34,20 +35,22 @@ class CheckoutSolution:
                 total_price += pricing[sku][0]
 
             pricing[sku] = (pricing[sku][0], 1 + pricing[sku][1])
-        breakpoint()
 
         if pricing["A"][1] > 0:
             # X the offer amount of 150 * how many times pricing is divisible by 3
             # then I need the remainder of the overall total occurances and then I need to add that by 50 
             # then add both values to get total amount
-            offers = (pricing["A"][1] // 3) * 130
+            if pricing["A"][1] % 3 == 0: 
+                offers = (pricing["A"][1] // 3) * 130
+
+            if pricing["A"][1] % 5 == 0:
+                offers = (pricing["A"][1] // 5) * 200
+
             remaining = 0
             if pricing["A"][1] % 3 != 0:
                 remaining = (pricing["A"][1] % 3) * 50
-            # breakpoint()
             total_price += offers + remaining
 
-        # Duplication probably not needed as we always have constant values on our hashmap
         if pricing["B"][1] > 0:
             offers = (pricing["B"][1] // 2) * 45
             remaining = 0
@@ -62,12 +65,10 @@ class CheckoutSolution:
 
             if pricing["E"][1] % 2 != 0:
                 remaining = (pricing["E"][1] % 2) * 30
-
-
             total_price += offers + remaining
 
-        # breakpoint()
         return total_price
+
 
 
 
