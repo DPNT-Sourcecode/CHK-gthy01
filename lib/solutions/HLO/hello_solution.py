a92ae9961bc2f612d282ea241d6933a49545e8f6
sbx_user1051 <sbx_user1051@169.254.19.121>
2025-06-25 18:32:08 +0000

    Wed Jun 25 18:32:08 UTC 2025

--- lib/solutions/HLO/hello_solution.py
+++ lib/solutions/HLO/hello_solution.py
@@ -9,9 +9,14 @@ class HelloSolution:
         """
         
         # Could add some handling around if str is empty
-        # if friend_name == "":
+        if friend_name == "":
+            # Ideally better to handle this Error more appropriately 
+                # - e.g. custom exceptions and error messaging which then would be asserted
+            # But for this simple test I think it's maybe overkill
+            raise(ValueError)
 
 
         return f"Hello {friend_name}"
+
 
 
